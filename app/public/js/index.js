c5109ed1815329d3987bc88912410fcefc010676
Preston McNabb <prestonmcnabb22@gmail.com>
2018-02-10 14:15:58 -0600

    fixed more error with the module not showing up

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,61 +1,7 @@
-// //const questions = [
-//   'You enjoy trying all types of food',
-//   'You enjoy binging the best new television shows',
-//   'You like to go running/biking outside',
-//   'You like tex-mex food',
-//   'You enjoy being social with others',
-//   'You also enjoy having alone time with yourself',
-//   'You like to study/work at coffee shops',
-//   'You love animals including reptiles',
-//   'Getting to travel to other countries sounds appealing',
-//   'You enjoing working hard and well as enjoying your time off',
-//   'You like to read a good book',
-//   'You believe friends can be made online through surveys like this'
-// //];
+$(document).ready(function () {
+    //your code here
 
-// const showMatch = function(match) {
-//   console.log(match);
-//   $('#match-modal')
-//     .html(
-//       `
-//       <div class="modal">
-//         <h3>Your match:</h3>
-//         <p class="match-name">${match.name}</p>
-//         <div class="match-img">
-//           <img src="${match.img}"/>
-//         </div>
-//       </div>
-//   `
-//     )
-//     .show();
-// };
-
-// questions.map(question => {
-//   $('.quiz').prepend(`
-//     <div class="center">
-//       <h3 class="center">${question}</h3>
-//         <div class="option">
-//           <select class="form-control" id="q1">
-//             <option disabled selected value> -- Select an Option -- </option>
-//             <option value="0">Strongly Disagree</option>
-//             <option value="1">Disagree</option>
-//             <option value="2">Neutral</option>
-//             <option value="3">Agree</option>
-//             <option value="4">Strongly Agree</option>
-//           </select>
-//         </div>
-//       </div>
-//     </div>
-//   `);
-// });
-// $('.quiz').prepend(`
-//   <div class="center">
-//     <label>Name:</label> <input type="text" name="name" required/><br />
-//     <label>Photo:</label> <input type="text" name="photo" required/>
-//   </div>
-// `);
-
-$("#findFriend").on("click", function(event){
+$('#findFriend').on("click", function(event){
 
     //We get the friends api
     $.get("/api/friends").done(function(data){
@@ -117,3 +63,4 @@ $("#findFriend").on("click", function(event){
             console.log("New Friend Added :)");
         });
     });
+});
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const jquery = require('jquery')
 
 //Create Express server
 const app = express();
