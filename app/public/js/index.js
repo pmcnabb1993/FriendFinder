7bf7377bdc7c594ff292f2b439746a6f05eabcfa
Preston McNabb <prestonmcnabb22@gmail.com>
2018-02-10 15:25:06 -0600

    Added jQuery.ajaxSettings.traditional = true to fix an issue with posting a new item in the friends array

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,4 +1,4 @@
-
+jQuery.ajaxSettings.traditional = true
     $('#findFriend').on("click", function(event){
 
         //We get the friends api
@@ -53,6 +53,7 @@
                 name: $("#name").val().trim(),
                 photo: $("#photo").val().trim(),
                 scores: [$("#q1").val(), $("#q2").val(), $("#q3").val(), $("#q4").val(), $("#q5").val(), $("#q6").val(), $("#q7").val(), $("#q8").val(), $("#q9").val(), $("#q10").val()]
+
             };
             console.log(newFriend);
     
