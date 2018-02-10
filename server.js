ff2bab219b71d5450e6132f9dcea018d042c507f
Preston McNabb <prestonmcnabb22@gmail.com>
2018-02-09 19:20:48 -0600

    minor updates

--- server.js
+++ server.js
@@ -11,15 +11,18 @@ const PORT = process.env.PORT || 3000;
 //Body Parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-//app.use('/img', express.static(path.join(__dirname, 'app/public/img')));
-//app.use('/js', express.static(path.join(__dirname, 'app/public/js')));
+app.use('/img', express.static(path.join(__dirname, 'app/public/img')));
+app.use('/js', express.static(path.join(__dirname, 'app/public/js')));
 app.use('/css', express.static(path.join(__dirname, 'app/public/css')));
+app.use('/fonts', express.static(path.join(__dirname, 'app/public/fonts')));
 
-require('./app/routing/apiRoutes.js');
-require('./app/routing/htmlRoutes.js');
+require('./app/routing/apiRoutes.js') (app);
+require('./app/routing/htmlRoutes.js') (app);
 
 
 app.listen(PORT, function() {
-    constole.log("FriendFinder is listening on port: " + PORT );
+    console.log("FriendFinder is listening on port: " + PORT );
 });
